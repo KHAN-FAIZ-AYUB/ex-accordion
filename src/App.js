450665ccd4d8103758576e09e2a615d21e7c3867
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-02-28 02:26:27 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function Accordion({ data }) {
   return (
     <div className="accordian">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} />
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
       ))}
     </div>
   );
@@ -41,7 +41,7 @@ const AccordionItem = ({ num, title, text }) => {
     return setIsOpen((isOpen) => !isOpen);
   };
   return (
-    <div className="item" onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "➖" : "➕"}</p>
