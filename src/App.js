6709428cff6a7d4a02e1608558da568e11ee5c71
Khan Faiz <khan.faiz.ayub@gmail.com>
2024-02-28 02:20:44 +0530

    handleToggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const faqs = [
   {
@@ -35,12 +35,17 @@ function Accordion({ data }) {
 }
 
 const AccordionItem = ({ num, title, text }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    return setIsOpen((isOpen) => !isOpen);
+  };
   return (
-    <div className="item">
+    <div className="item" onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">➖</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{isOpen ? "➖" : "➕"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 };
